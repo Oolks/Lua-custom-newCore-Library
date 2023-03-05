377a8cd8bf7d5735d5b4f91607f933ec3c1d0ef6
Oolks <102543055+OoIks@users.noreply.github.com>
2023-03-05 11:09:22 +0500

    Update __MAIN.lua

--- __MAIN.lua
+++ __MAIN.lua
@@ -6,127 +6,110 @@
 
 local newCore = {}
 
-
 function newCore.wait(seconds)
-    if seconds == nil or type(seconds) == "string" then
+    if type(seconds) ~= "number" or seconds <= 0 then
         return false
-        else
-            if seconds > 0 then
-                local start = os.time()
-                repeat until os.time() > start + seconds
-        end
     end
+    
+    local start = os.time()
+    repeat until os.time() > start + seconds
 end
 
 function newCore.InstanceNew(name, typeOFile, ToWrite, useWplus)
-    if name and typeOFile and ToWrite then
-        local filenameCombined = name..typeOFile
-        local strdom = "."
-        local useOfnew = false --default
-        if string.find(typeOFile, ".") then
-            useOfnew = true
-        end
-        if useOfnew == true then
-            print("Building a file named "..name.." as a "..typeOFile)
-            elseif useOfnew == false then
-                print("Building a file named "..name.." as a ."..typeOFile.." file")
-            end
-
-            if useWplus == true then 
-               file = io.open(filenameCombined, "w+")
-                io.input(file)
-                io.output(file)
-                io.write(ToWrite)
-                io.close(file)
-                return true
-                else
-                    
-            
-            file1 = io.open(filenameCombined, "w")
-            io.input(file1)
-            io.output(file1)
-            io.write(ToWrite) 
-            io.close()
-            return true
-            end
-        else
-                return false
-    end
-end
-
-
-function newCore.Join(value, value1)
-    if value == true or false then 
+    if not name or not typeOFile or not ToWrite then
         return false
     end
-    if value  then
-       local newValue = print(value..value1)
-       return newValue 
+    
+    if not string.find(typeOFile, "%.") then
+        typeOFile = "." .. typeOFile
     end
+    
+    print("Building a file named " .. name .. " as a " .. typeOFile)
+    
+    local mode = useWplus and "w+" or "w"
+    local file = io.open(name .. typeOFile, mode)
+    if not file then
+        return false
+    end
+    
+    io.write(file, ToWrite)
+    io.close(file)
+    
+    return true
+end
+
+function newCore.Join(value, value1)
+    if type(value) ~= "string" or type(value1) ~= "string" then
+        return false
+    end
+    
+    print(value .. value1)
+    return true
 end
 
 function newCore.ReturnUpdateInt()
     return 0.2
 end
 
-
-function newCore.GetARandomNumber(from,to)
-    return math.rad(from,to)
+function newCore.GetARandomNumber(from, to)
+    return math.random(from, to)
 end
-
 
 function newCore.GenerateRandomString(length)
-    if  length > 4 then
-        local RandomGenerated = ""
-        for i = 1, length do
-            RandomGenerated = RandomGenerated .. string.char(math.random(97, 122))
-            if #RandomGenerated == length then
-                return RandomGenerated
-            end
-        end
+    if length < 5 then
+        return false
     end
+    
+    local chars = {}
+    for i = 97, 122 do
+        chars[#chars + 1] = string.char(i)
+    end
+    
+    local randomStr = ""
+    for i = 1, length do
+        randomStr = randomStr .. chars[math.random(#chars)]
+    end
+    
+    return randomStr
 end
-
 
 function newCore.GenerateRandomizedKeyCodes()
-    local block1 = math.random(0, 0xffff)
-    local block2 = math.random(0, 0xffff)
-    local block3 = math.random(0, 0xffff)
-    local block4 = math.random(0, 0xffff)
-    local block5 = math.random(0, 0xffff)
-    local block6 = math.random(0, 0xffff)
+    return string.format("%04x%04x-%04x-%04x-%04x-%04x%04x%04x",
+        math.random(0, 0xffff), math.random(0, 0xffff),
+        math.random(0, 0xffff), math.random(0, 0xfff) + 0x4000,
+        math.random(0, 0x3fff) + 0x8000, math.random(0, 0xffff), math.random(0, 0xffff))
+end
+
+function newCore.GetTotalValueLength(value)
+    if type(value) ~= "string" then
+        return false
+    end
     
-    return string.format("%04x%04x-%04x-%04x-%04x-%04x%04x%04x", block1, block2, block3, block4, block5, block6, block7, block8)
+    return #value
 end
-
-
-function newCore.GetTotalValueLenght(type1, value)
-    if type1 == "string" and value then
-        return #value
-        else
-            return false
-        end
-end
-
 
 function newCore.RetrieveLuaVersion()
     return _VERSION
 end
 
-
 function newCore.CleanTable(TableVariable, safeMode)
-    if safeMode == true then
-        for p in pairs(TableVariable)do
+    if type(TableVariable) ~= "table" then
+        return false
+    end
+    
+    if safeMode then
+        for k in pairs(TableVariable) do
             newCore.wait(0.2)
-            TableVariable[p] = nil
+            TableVariable[k] = nil
+        end
+    else
+        for k in pairs(TableVariable) do
+            TableVariable[k] = nil
         end
     end
-    if safeMode == false then
-    for k in pairs (TableVariable) do
-        TableVariable[k] = nil
-        return true
-    end
-    end
+    
+    return true
 end
 
 return newCore
+
